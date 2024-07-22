7e36d180d84e25b8f25255609370d4f0d903d17f
Luke Hunter <luke.hunter9@gmail.com>
2024-07-22 19:11:55 +1000

    Set up models

---
+++ server/models/Offer.js
@@ -0,0 +1,19 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema for the offers. It is used as the schema for the User's 'savedOffers' array.
+const offerSchema = new Schema({
+    description: {
+        type: String,
+        required: true,
+      },
+    offerType: {
+        type: String,
+        required: true,
+    },
+    stamps: {
+        type: Number,
+        default: 8
+    }
+})
+
+module.exports = offerSchema
